13ee2c869207fbec4b989fd6911b66455e14ed42
Andrew Jones <ajon542@gmail.com>
2016-08-23 15:53:06 -0700

    Added project framework

---
+++ MergeSort/MergeSortTest/MergeSortTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MergeSortTest
+{
+    [TestClass]
+    public class MergeSortTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
