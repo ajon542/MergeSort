ff3aa8e0b3ac1aa62a1c844554686e07068cf839
Andrew Jones <ajon542@gmail.com>
2016-08-23 18:52:55 -0700

    Added an easier to understand merge sort
    Though, it comes with the inefficiency of copying a list for each
    recursion.

--- MergeSort/MergeSort/MergeSort.cs
+++ MergeSort/MergeSort/MergeSort.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.Collections.Generic;
+using System.Linq;
+
 namespace Sorting
 {
     /// <summary>
@@ -6,6 +8,8 @@ namespace Sorting
     /// </summary>
     public class MergeSort
     {
+        #region Array Implementation
+
         public static void MergeSort_Recursive(int[] input)
         {
             MergeSort_Recursive(input, 0, input.Length, new int[input.Length]);
@@ -48,5 +52,67 @@ namespace Sorting
                 }
             }
         }
+
+        #endregion
+
+        #region List Implementation
+
+        public static List<int> MergeSort_Recursive(List<int> input)
+        {
+            if (input.Count <= 1)
+            {
+                return input;
+            }
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+            for(int i = 0; i < input.Count; ++i)
+            {
+                if (i % 2 == 0)
+                {
+                    right.Add(input[i]);
+                }
+                else
+                {
+                    left.Add(input[i]);
+                }
+            }
+
+            left = MergeSort_Recursive(left);
+            right = MergeSort_Recursive(right);
+
+            return Merge(left, right);
+        }
+
+        private static List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> result = new List<int>();
+
+            int l = 0;
+            int r = 0;
+            while (l < left.Count && r < right.Count)
+            {
+                if (left[l] <= right[r])
+                {
+                    result.Add(left[l++]);
+                }
+                else
+                {
+                    result.Add(right[r++]);
+                }
+            }
+
+            while (l < left.Count)
+            {
+                result.Add(left[l++]);
+            }
+            while (r < right.Count)
+            {
+                result.Add(right[r++]);
+            }
+            return result;
+        }
+
+        #endregion
     }
 }
--- MergeSort/MergeSortTest/MergeSortTest.cs
+++ MergeSort/MergeSortTest/MergeSortTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Sorting;
@@ -68,6 +69,76 @@ namespace MergeSortTest
             }
 
             MergeSort.MergeSort_Recursive(input);
+
+            for (int i = 0; i < ItemCount - 1; ++i)
+            {
+                if (input[i] > input[i + 1])
+                {
+                    Assert.Fail("input element input[{0}]={1} is greater than input[{2}]={3}", i, input[i], i + 1, input[i + 1]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            List<int> input = new List<int> { 1 };
+            List<int> expected = new List<int> { 1 };
+
+            input = MergeSort.MergeSort_Recursive(input);
+
+            CollectionAssert.AreEqual(expected, input);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            List<int> input = new List<int> { 2, 1 };
+            List<int> expected = new List<int> { 1, 2 };
+
+            input = MergeSort.MergeSort_Recursive(input);
+
+            CollectionAssert.AreEqual(expected, input);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            List<int> input = new List<int> { 3, 2, 1 };
+            List<int> expected = new List<int> { 1, 2, 3 };
+
+            input = MergeSort.MergeSort_Recursive(input);
+
+            CollectionAssert.AreEqual(expected, input);
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            List<int> input = new List<int> { 3, 2, 1, 10 };
+            List<int> expected = new List<int> { 1, 2, 3, 10 };
+
+            input = MergeSort.MergeSort_Recursive(input);
+
+            CollectionAssert.AreEqual(expected, input);
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            const int ItemCount = 1000;
+
+            Random rnd = new Random();
+
+            List<int> input = new List<int>();
+
+            for (int i = 0; i < ItemCount; ++i)
+            {
+                int random = rnd.Next(ItemCount);
+                input.Add(random);
+            }
+
+            input = MergeSort.MergeSort_Recursive(input);
 
             for (int i = 0; i < ItemCount - 1; ++i)
             {
