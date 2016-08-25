ec94a7cc991ca4e744764f8a8dc49c36af9ae855
Andrew Jones <ajon542@gmail.com>
2016-08-24 20:06:35 -0700

    Added quicksort implementation and unit tests

---
+++ MergeSort/MergeSort/QuickSort.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class QuickSort
+    {
+        public static List<int> Sort(List<int> input)
+        {
+            return Sort(input, 0, input.Count - 1);
+        }
+
+        private static List<int> Sort(List<int> input, int left, int right)
+        {
+            int index = 0;
+
+            if (input.Count > 1)
+            {
+                index = Partition(input, left, right);
+
+                if (left < index - 1)
+                {
+                    Sort(input, left, index - 1);
+                }
+
+                if (index < right)
+                {
+                    Sort(input, index, right);
+                }
+            }
+
+            return input;
+        }
+
+        private static int Partition(List<int> input, int left, int right)
+        {
+            int pivot = input[(left + right) >> 1];
+            int i = left;
+            int j = right;
+
+            while (i <= j)
+            {
+                while (input[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (input[j] > pivot)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    Swap(input, i, j);
+
+                    i++;
+                    j--;
+                }
+            }
+
+            return i;
+        }
+
+        private static void Swap(List<int> input, int a, int b)
+        {
+            int tmp = input[a];
+            input[a] = input[b];
+            input[b] = tmp;
+        }
+    }
+}
---
+++ MergeSort/MergeSortTest/QuickSortTest.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Sorting;
+
+
+namespace MergeSortTest
+{
+    [TestClass]
+    public class QuickSortTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<int> input = new List<int> { 1 };
+            List<int> expected = new List<int> { 1 };
+
+            input = QuickSort.Sort(input);
+
+            CollectionAssert.AreEqual(expected, input);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            List<int> input = new List<int> { 2, 1 };
+            List<int> expected = new List<int> { 1, 2 };
+
+            input = QuickSort.Sort(input);
+
+            CollectionAssert.AreEqual(expected, input);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            List<int> input = new List<int> { 3, 2, 1 };
+            List<int> expected = new List<int> { 1, 2, 3 };
+
+            input = QuickSort.Sort(input);
+
+            CollectionAssert.AreEqual(expected, input);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            List<int> input = new List<int> { 4, 3, 2, 1 };
+            List<int> expected = new List<int> { 1, 2, 3, 4 };
+
+            input = QuickSort.Sort(input);
+
+            CollectionAssert.AreEqual(expected, input);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            List<int> input = new List<int> { 1, 2, 3, 4 };
+            List<int> expected = new List<int> { 1, 2, 3, 4 };
+
+            input = QuickSort.Sort(input);
+
+            CollectionAssert.AreEqual(expected, input);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            const int ItemCount = 1000;
+
+            Random rnd = new Random();
+
+            List<int> input = new List<int>();
+
+            for (int i = 0; i < ItemCount; ++i)
+            {
+                int random = rnd.Next(ItemCount);
+                input.Add(random);
+            }
+
+            input = QuickSort.Sort(input);
+
+            for (int i = 0; i < ItemCount - 1; ++i)
+            {
+                if (input[i] > input[i + 1])
+                {
+                    Assert.Fail("input element input[{0}]={1} is greater than input[{2}]={3}", i, input[i], i + 1, input[i + 1]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            const int ItemCount = 1001;
+
+            Random rnd = new Random();
+
+            List<int> input = new List<int>();
+
+            for (int i = 0; i < ItemCount; ++i)
+            {
+                int random = rnd.Next(ItemCount);
+                input.Add(random);
+            }
+
+            input = QuickSort.Sort(input);
+
+            for (int i = 0; i < ItemCount - 1; ++i)
+            {
+                if (input[i] > input[i + 1])
+                {
+                    Assert.Fail("input element input[{0}]={1} is greater than input[{2}]={3}", i, input[i], i + 1, input[i + 1]);
+                }
+            }
+        }
+    }
+}
